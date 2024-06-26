1a9d38f8f67f25766efb886ce82aa613a4a6ecd3
ReginaldoGaspar <reginaldogaspar@gmail.com>
2024-06-25 21:31:05 -0300

    Método cadastrarProduto alterado para retornar String. Será usado o rettorno como mensagem do BD

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -7,7 +7,6 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
@@ -15,42 +14,34 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
+
+    public String cadastrarProduto(ProdutosDTO produto) {
+
         conn = new conectaDAO().connectDB();
-        
+
         try {
             prep = conn.prepareStatement("INSERT INTO produtos VALUES(?,?,?,?)");
-            prep.setInt(1,0);// para o sgbd controlar o id
+            prep.setInt(1, 0);// para o sgbd controlar o id
             prep.setString(2, produto.getNome());
             prep.setDouble(3, produto.getValor());
             prep.setString(4, produto.getStatus());
             prep.executeUpdate();
-
+            return "Cadastro realizado com sucesso!";
         } catch (SQLException ex) {
-            System.out.println("Erro ao conectar: " + ex.getMessage());
+            return "Erro ao cadastrar!";
         }
 
-
-        
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}

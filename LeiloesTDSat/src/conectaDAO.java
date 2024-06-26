2be6b0a6f35d1fe78679cf6ca17a95f530eaff72
ReginaldoGaspar <reginaldogaspar@gmail.com>
2024-06-26 08:26:22 -0300

    Correção de bug na classe conectaDAO(autoReconnect=true&useSSL=false) para evitar mensagem do SGBD

--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -17,12 +17,17 @@ import javax.swing.JOptionPane;
  */
 public class conectaDAO {
     
+    public static String url = "jdbc:mysql://localhost:3306/uc11?" + "autoReconnect=true&useSSL=false"; /*+ "autoReconnect=true&useSSL=false"  - Usei para evitar mensagem Establishing
+                                                                                        SSL connection without server's identity verification is not recommended” warning when connecting */
+    public static String user = "root"; //nome do usuário do MySQL
+    public static String password = "elge4ds5"; //senha do MySQL 
+    
     public Connection connectDB(){
         Connection conn = null;
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=elge4ds5");
+           conn = DriverManager.getConnection(url, user, password);
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
